7beb58fc406a1d65d7cb978a1e964e3f37c50324
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-03-04 22:41:26 +0300

    commit my first js code

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,12 @@
+export function taskFirst(){
+    const task = "I prefer const when I can."
+    return task
+}
+
+export function taskNext(){
+    let task = "But sometimes let is okay"
+    return task
+}
+
+taskFirst()
+taskNext()
