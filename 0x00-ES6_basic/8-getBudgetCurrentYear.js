1b33b92d80444499adbdd1b6b527b908c4d65f46
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-03-08 20:06:41 +0300

    task 8 and trial for eslint

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,18 +1,13 @@
-function getCurrentYear() {
+function getCurrentYear () {
   const date = new Date();
   return date.getFullYear();
 }
 
-export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {};
-
-  budget{
-    budget[`income-${getCurrentYear()}`] = income;
-    budget[`gdp-${getCurrentYear()}`] = gdp;
-    budget[`capita-${getCurrentYear()}`] = capita;
-  }
-
-  return Object.values(budget);
-}
-
-safety 
+export default function getBudgetForCurrentYear (income, gdp, capita) {
+  const year = getCurrentYear();
+  return {
+    [`income-${year}`]: income,
+    [`gdp-${year}`]: gdp,
+    [`capita-${year}`]: capita
+  };
+};
