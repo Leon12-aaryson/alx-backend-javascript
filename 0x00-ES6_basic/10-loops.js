bca0f875eaa49fda455fa21f3122f226f4f5bf2b
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-04-30 23:50:42 +0300

    initiate tasks on nodejs basics

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
 	const arr = []
 	for (const idx of array) {
-		arr.push(appendString + idx)
+		arr.push(appendString + idx);
 	}
 
-	return arr
+	return arr;
 }
---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Welcome to Holberton School, what is your name?\n', (input) => {
+  console.log(`Your name is: ${input}`);
+  console.log('This important software is now closing\n');
+  rl.close();
+});
+
+rl.on("close", function () {
+  console.log("\nExiting the program, goodbye!");
+  process.exit(0);
+});
+
