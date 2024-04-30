c6c092d56c0e1a79ee4f9330f5d9eed958a25eb1
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-05-01 00:34:55 +0300

    correct errors of semistandard

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-function countStudents(path) {
+function countStudents (path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', (error, data) => {
       if (error) {
@@ -11,7 +11,7 @@ function countStudents(path) {
       const lines = data.trim().split('\n');
       let mathCount = 0;
       let CSCount = 0;
-      let fields = {};
+      const fields = {};
 
       for (const line of lines) {
         const studentData = line.split(',');
@@ -21,19 +21,19 @@ function countStudents(path) {
 
           if (field === 'Math') {
             mathCount++;
-            if (!fields['Math']) fields['Math'] = [];
-            fields['Math'].push(firstName);
+            if (!fields.Math) fields.Math = [];
+            fields.Math.push(firstName);
           } else if (field === 'CS') {
             CSCount++;
-            if (!fields['CS']) fields['CS'] = [];
-            fields['CS'].push(firstName);
+            if (!fields.CS) fields.CS = [];
+            fields.CS.push(firstName);
           }
         }
       }
 
       console.log(`Number of students: ${mathCount + CSCount}`);
-      console.log(`Number of students in Math: ${mathCount}. List: ${fields['Math'].join(', ')}`);
-      console.log(`Number of students in CS: ${CSCount}. List: ${fields['CS'].join(', ')}`);
+      console.log(`Number of students in Math: ${mathCount}. List: ${fields.Math.join(', ')}`);
+      console.log(`Number of students in CS: ${CSCount}. List: ${fields.CS.join(', ')}`);
 
       resolve();
     });
