d9b94728eb79bcd35d3a3d66bec3939c15545ef6
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-05-01 00:16:41 +0300

    semistandard fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,11 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data.toString()}`);
 
-rl.question('Welcome to Holberton School, what is your name?\n', (input) => {
-  console.log(`Your name is: ${input}`);
-  console.log('This important software is now closing\n');
-  rl.close();
-});
-
-rl.on('close', function () {
-  console.log('\nExiting the program, goodbye!');
-  process.exit(0);
+  // Check if input is coming from a terminal (TTY)
+  if (!process.stdin.isTTY) {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
 });
