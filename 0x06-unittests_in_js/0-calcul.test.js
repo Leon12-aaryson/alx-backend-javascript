ccc0864d7701c535a8e86055781d21e0ef25aa84
Leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2024-05-02 22:10:17 +0300

    readjusting the package.json file

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,16 +2,16 @@ const calculateNumber = require('./0-calcul.js');
 const assert = require('assert');
 
 describe('calculateNumber', function () {
-  it('round positive numbers correctly', function () {
+  it('should round positive numbers correctly', function () {
     assert.equal(calculateNumber(3.7, 2.1), 6);
   });
-  it('round negative numbers correctly', function () {
+  it('should round negative numbers correctly', function () {
     assert.equal(calculateNumber(-3.7, -2.1), -6);
   });
   it('dealing with the zero case', function () {
     assert.equal(calculateNumber(0.0, 0.0), 0);
   });
-  it('work with whole numbers', function () {
+  it('should work with whole numbers', function () {
     assert.equal(calculateNumber(2, 3), 5);
   });
 });
